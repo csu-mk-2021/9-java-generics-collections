060f4ae32d4d3a295d9d4eaf3eb35b40237df902
AntyufeevA <awsd.artem.2k00@gmail.com>
2021-11-29 16:53:38 +0500

    task01 []

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,45 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public final class Pair <T1, T2> {
+    private final T1 value1;
+    private final T2 value2;
+
+    private Pair(T1 value1, T2 value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 value1, T2 value2) {
+        return new Pair<>(value1, value2);
+    }
+
+    public T1 getFirst() {
+        return value1;
+    }
+
+    public T2 getSecond() {
+        return value2;
+    }
+
+    public boolean equals(Pair pair) {
+        return this.value1 == pair.value1 && this.value2 == pair.value2;
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Objects.hashCode(value1);
+        result = prime * result + Objects.hashCode(value2);
+        return result;
+    }
+
+
+    public void ifPresent(BiConsumer<? super T1, ? super T2> biConsumer) {
+        if (value1 != null && value2 != null) {
+            biConsumer.accept(value1, value2);
+        }
+    }
 }
