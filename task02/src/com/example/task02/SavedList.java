e5a1881d631800f11f57d9dcbe3e42bc3da51867
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-12-20 11:52:20 +0500

    task02 successful

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -3,33 +3,71 @@ package com.example.task02;
 import java.io.File;
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
+    private List<E> files;
+    private final File copyFile;
 
     public SavedList(File file) {
+        this.copyFile = file;
+        if (file.exists()) {
+            loadFileCopy();
+        } else {
+            this.files = new ArrayList<>();
+        }
+    }
+
+    private void saveFileCopy() {
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(this.copyFile));
+            stream.writeObject(this.files);
+            stream.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    private void loadFileCopy() {
+        try {
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(this.copyFile));
+            this.files = (ArrayList<E>) stream.readObject();
+            stream.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return this.files.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E elem = this.files.set(index, element);
+        saveFileCopy();
+        return elem;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.files.size();
     }
 
     @Override
     public void add(int index, E element) {
+        this.files.add(index, element);
+        saveFileCopy();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E indexRemove = this.files.get(index);
+        this.files.remove(indexRemove);
+        saveFileCopy();
+        return indexRemove;
     }
 }
